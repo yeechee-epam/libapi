249d83b578321104d1a2a14bf46ae254a2ab1ae3
yeechee-epam <yeechee_yong@epam.com>
2026-01-12 14:36:34 +0800

    first commit

---
+++ src/test/java/com/example/libapi/LibapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
