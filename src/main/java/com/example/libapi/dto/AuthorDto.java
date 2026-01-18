aff9016692b381a00b528bb967eefab01a24a77e
chee <yeechee_yong@epam.com>
2026-01-19 03:36:11 +0800

    KAN-30 add author feature: KAN-39 POST /authors endpoint

--- src/main/java/com/example/libapi/dto/AuthorDto.java
+++ src/main/java/com/example/libapi/dto/AuthorDto.java
@@ -1,5 +1,7 @@
 package com.example.libapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -7,6 +9,8 @@ import java.util.List;
 @Data
 public class AuthorDto {
     private Long id;
+    @NotBlank(message = "Author name is required")
+    @Size(max = 50, message = "Author name must be <= 50 characters")
     private String name;
     //    kan-45 view author page
     private List<BookSummaryDto> books; // Add this
@@ -14,6 +18,7 @@ public class AuthorDto {
     @Data
     public static class BookSummaryDto {
         private Long id;
+
         private String name;
         private String bookLink;//to direct to book page
 
